9480e5c68888e00a2ca74420d878b5b355ed5007
Avinash Pratap Singh <avinashpratap0074@gmail.com>
2025-11-12 00:21:42 +0530

    Add files via upload

---
+++ Interleave.java
@@ -0,0 +1,35 @@
+package queue;
+import java.util.*;
+
+public class Interleave {
+    public static void InterLeave(Queue<Integer> q) {
+        Queue<Integer> FirstHalf = new LinkedList<>();
+        int size = q.size();
+        for(int i = 0; i<size/2; i++) {
+            FirstHalf.add(q.remove());
+        }
+        while(!FirstHalf.isEmpty()) {
+            q.add(FirstHalf.remove());
+            q.add(q.remove());
+        } 
+    }
+    public static void main(String[] args) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        q.add(5);
+        q.add(6);
+        q.add(7);
+        q.add(8);
+        q.add(9);
+        q.add(10);
+
+        InterLeave(q);
+        while(!q.isEmpty()) {
+            System.out.print(q.remove()+" ");
+        } 
+        System.out.println();
+    }
+}
